e5ddbac449720061181480ce5c707dbc30c9090a
Benalgins <simeon.ivanew@gmail.com>
2025-04-05 15:30:06 +0300

    Added home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,17 @@
 function Home() {
 	return (
 		<div className="section home">
-			<div className="about"></div>
+			<div className="about">
+				<p>
+					Register to compete in the leaderboard by guessing or adding champions
+					and acquiring points on GuessMyChamp, where your predictions and
+					knowledge earn you a spot at the top!
+				</p>
+			</div>
+			<div className="try-demo">
+				<p>Jump in and take a demo run at guessing champions</p>
+				<button>Demo Run</button>
+			</div>
 		</div>
 	);
 }
