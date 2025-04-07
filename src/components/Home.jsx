79a0798cc263c883167358c8e9e767b7564d22d4
Benalgins <simeon.ivanew@gmail.com>
2025-04-07 19:54:11 +0300

    Styled Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,11 +2,19 @@ function Home() {
 	return (
 		<div className="section home">
 			<div className="about">
+				<div className="wellcome-img">
+					<img src="../../public/Images/wellcome-pengu.png" alt="" />
+				</div>
 				<p>
+					Wellcome to GuessMychamp, if your a loving fan of League of Legends
+					and want to test your knowladge of champions this is the right place
+					for you!
+				</p>
+				{/* <p>
 					Register to compete in the leaderboard by guessing or adding champions
 					and acquiring points on GuessMyChamp, where your predictions and
 					knowledge earn you a spot at the top!
-				</p>
+				</p> */}
 			</div>
 			<div className="try-demo">
 				<p>Jump in and take a demo run at guessing champions</p>
